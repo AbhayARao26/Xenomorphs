6a7c4fb1fad0405aaba13293c4d107ef844682bd
Abhay A Rao <iabhayrao@gmail.com>
2024-08-21 19:57:06 +0530

    All components optimised and updated

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< Updated upstream
-  assetsInclude: ['**/*.glb']
-=======
   assetsInclude: ['**/*.glb', '**/*.hdr']
->>>>>>> Stashed changes
 })
-
